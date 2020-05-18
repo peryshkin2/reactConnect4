47749feeaccd9486a8a606bf96a8d242a091c096
peryshkin2 <peryshkin@gmail.com>
2020-05-17 18:09:20 -0700

    replace init to cycles over rows and columns

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const fullBoard = 42;
+const rowWidth = 7;
+const columnDepth = 6;
 
 function HeadSquare(props) {
     return (
@@ -38,76 +41,42 @@ class Board extends React.Component {
     );
   }
   
-  render() {
+  renderHeadRow() {   
+   let anyRow = Array.from(Array(rowWidth).keys());
+    return( anyRow.map((item) =>{
+      return (
+        this.renderHead(item)
+      );
+    }));
+  }
+
+  renderRow(j) {   
+   let anyRow = Array.from(Array(rowWidth).keys());
+    return( anyRow.map((item) =>{
+      return (
+        this.renderSquare(j*rowWidth+item)
+      );
+    }));
+  }
   
+  renderColumns() {
+  let anyColumn = Array.from(Array(columnDepth).keys());
+    return( anyColumn.map((item)=>{
+      return(
+        <div className="board-row">
+          {this.renderRow(item)}
+        </div>
+      );
+    }));
+  }
+
+  render() {
     return (
       <div>
         <div className="board-row">
-          {this.renderHead(0)}
-          {this.renderHead(1)}
-          {this.renderHead(2)}
-          {this.renderHead(3)}
-          {this.renderHead(4)}
-          {this.renderHead(5)}
-          {this.renderHead(6)}
-        </div>
-        
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-
-        </div>
-        <div className="board-row">
-	  {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-        </div>
-<div className="board-row">
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
-          {this.renderSquare(20)}
-        </div>
-<div className="board-row">
-          {this.renderSquare(21)}
-          {this.renderSquare(22)}
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
-          {this.renderSquare(25)}
-          {this.renderSquare(26)}
-          {this.renderSquare(27)}
-        </div>
-<div className="board-row">
-          {this.renderSquare(28)}
-          {this.renderSquare(29)}
-          {this.renderSquare(30)}
-          {this.renderSquare(31)}
-          {this.renderSquare(32)}
-          {this.renderSquare(33)}
-          {this.renderSquare(34)}
-        </div>
-<div className="board-row">
-          {this.renderSquare(35)}
-          {this.renderSquare(36)}
-          {this.renderSquare(37)}
-          {this.renderSquare(38)}
-          {this.renderSquare(39)}
-          {this.renderSquare(40)}
-          {this.renderSquare(41)}
-        </div>
-
+          {this.renderHeadRow()}
+        </div>       
+        {this.renderColumns()}       
       </div>
     );
   }
@@ -121,7 +90,7 @@ class Game extends React.Component {
  //   const rowWidth = 7;
     this.state = {
       history: [{
-        squares: Array(fullBoard).fill(null),      }],
+        squares: Array(fullBoard).fill(null), }],
       stepNumber: 0,
       xIsNext: true,
       winner: null,
@@ -215,8 +184,7 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-const fullBoard = 42;
-const rowWidth = 7;
+
 function calculateWinner(squares,j) {
   
   const curType = squares[j];
